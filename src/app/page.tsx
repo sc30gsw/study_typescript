8086fa2318259d1b6330083598e16e2fdddc185a
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-20 07:25:33 +0900

    🏷️ String Literal Types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
 const Home = () => {
+  return (
+    <div>
+      <Component type="a" />
+    </div>
+  )
+}
+
+const Component: React.FC<{ type: 'a' }> = ({ type }) => {
+  if (type === 'a') return <div>a</div>
+
   return <div>test</div>
 }
 

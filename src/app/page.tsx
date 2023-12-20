630d650d6bcf733525f4008accda4e5db055c962
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-21 06:19:56 +0900

    🏷️ オブジェクトの型付

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-const Home = () => {
-  return (
-    <div>
-      <Component foo />
-    </div>
-  )
+let obj1: Record<string, unknown> = {}
+let obj2: { [key: string]: unknown } = {}
+
+let obj3: Record<string, number> = { a: 1, b: 2 }
+let obj4: { [key: number]: string; foo: 'foo' } = {
+  1: 'hoge',
+  2: 'foo',
+  foo: 'foo',
 }
 
-const Component: React.FC<{ foo?: true }> = ({ foo }) => {
-  if (foo) return <div>a</div>
-
+const Home = () => {
   return <div>test</div>
 }
 

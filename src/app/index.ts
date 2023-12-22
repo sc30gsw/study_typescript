77514a6b985eec523049fa868e0e62377c731f77
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-22 09:45:15 +0900

    🏷️ 関数のGenerics

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,37 +1,6 @@
-type User<T> = {
-  name: string
-  state: T
+const foo = <T>(arg: T) => {
+  return { value: arg }
 }
 
-type Japanese = User<'東京都' | '大阪府'>
-type American = User<'CA' | ' NY'>
-
-const user1: Japanese = {
-  name: '田中',
-  state: '東京都',
-}
-
-const user2: American = {
-  name: 'john',
-  state: 'CA',
-}
-
-type Foo<T extends string | boolean = string> = {
-  value: T
-}
-
-const foo: Foo = {
-  value: 'foo',
-}
-
-const foo1: Foo<string> = {
-  value: 'hoge',
-}
-
-const foo2: Foo<boolean> = {
-  value: false,
-}
-
-const foo3: Foo<'bar'> = {
-  value: 'bar',
-}
+const foo1 = foo<number[]>([1, 2, 3])
+const foo2 = foo<{ foo: string[] }>({ foo: ['0', '1'] })

c4c43550692e3e289ab77f368971e975f6f06df2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-22 09:54:22 +0900

    🏷️ ユーザー定義の型ガードとの組み合わせ

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,15 @@
-const foo = <T>(arg: T) => {
-  return { value: arg }
+const isString = (arg: string | number): arg is string => {
+  return typeof arg === 'string'
 }
 
-const foo1 = foo<number[]>([1, 2, 3])
-const foo2 = foo<{ foo: string[] }>({ foo: ['0', '1'] })
+const foo = <T extends string | number>(arg: T) => {
+  if (isString(arg)) {
+    return { value: arg.toUpperCase() }
+  }
+
+  return { value: arg.toFixed() }
+}
+
+// const foo1 = foo<number[]>([1, 2, 3])
+// const foo2 = foo<{ foo: string[] }>({ foo: ['0', '1'] })
+const foo1 = foo('hoge')
